4d7d32a7b5a4a5b2aab212da0e4bb6cf1ba297d0
RexFun <oleFun@icloud.com>
2016-01-26 15:15:21 +0800

    Initial Commit

---
+++ UINavigationController3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UINavigationController3
+//
+//  Created by mac373 on 16/1/26.
+//  Copyright © 2016年 ole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UINavigationController3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UINavigationController3
+//
+//  Created by mac373 on 16/1/26.
+//  Copyright © 2016年 ole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
